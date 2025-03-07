5a77234b3213c2d1f91c37f7abd18f81ffb1f888
Lulu <lululogicz@gmail.com>
2025-03-07 15:28:17 +0000

    Update duplaication

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -3,40 +3,21 @@ import { getImageUrl } from '../../utils';
 import styles from './ProjectCard.module.css';
 
 export const ProjectCard = ({
-  project: { title, imageSrc, description, skills, demo, source }
+  project: { title, imageSrc, description }
 }) => {
   return (
     <div className={styles.container}>
-      {/* Left or top: Image */}
-      <div className={styles.imageContainer}>
-        <img
-          src={getImageUrl(imageSrc)}
-          alt={`image of ${title}`}
-          className={styles.image}
-        />
-      </div>
+      {/* Large Image */}
+      <img
+        src={getImageUrl(imageSrc)}
+        alt={`image of ${title}`}
+        className={styles.image}
+      />
 
-      {/* Right or bottom: Text content */}
-      <div className={styles.content}>
+      {/* Title & Subtitle only */}
+      <div className={styles.text}>
         <h3 className={styles.title}>{title}</h3>
         <p className={styles.subtitle}>{description}</p>
-
-        <ul className={styles.skills}>
-          {skills.map((skill, idx) => (
-            <li key={idx} className={styles.skill}>
-              {skill}
-            </li>
-          ))}
-        </ul>
-
-        <div className={styles.links}>
-          <a href={demo} className={styles.link}>
-            Demo
-          </a>
-          <a href={source} className={styles.link}>
-            Source code
-          </a>
-        </div>
       </div>
     </div>
   );
