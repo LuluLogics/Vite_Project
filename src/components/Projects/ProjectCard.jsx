0a9ff0ae0cc936dfe6ec17d1bad2e45db720154f
Lulu <lululogicz@gmail.com>
2025-03-07 15:21:57 +0000

    Individual card

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,34 +1,43 @@
-import React from 'react'
+import React from 'react';
 import { getImageUrl } from '../../utils';
-import styles from "./ProjectCard.module.css"
+import styles from './ProjectCard.module.css';
 
 export const ProjectCard = ({
-    project : {title, imageSrc, description, skills, demo, source
-}}) => {
+  project: { title, imageSrc, description, skills, demo, source }
+}) => {
   return (
     <div className={styles.container}>
-        <img 
-            src={getImageUrl (imageSrc)} 
-            alt={`image of ${title}`}
-            className={styles.image}
+      {/* Left or top: Image */}
+      <div className={styles.imageContainer}>
+        <img
+          src={getImageUrl(imageSrc)}
+          alt={`image of ${title}`}
+          className={styles.image}
         />
+      </div>
+
+      {/* Right or bottom: Text content */}
+      <div className={styles.content}>
         <h3 className={styles.title}>{title}</h3>
-        <p className={styles.description}>{description}</p>
-        <ul className={styles.skills}>{
-            skills.map((skill, id) => {
-                return (
-                    <li key={id} className={styles.skill}>{skill}</li>
-                );
-            })}
+        <p className={styles.subtitle}>{description}</p>
+
+        <ul className={styles.skills}>
+          {skills.map((skill, idx) => (
+            <li key={idx} className={styles.skill}>
+              {skill}
+            </li>
+          ))}
         </ul>
+
         <div className={styles.links}>
-            <a href={demo} className={styles.link}>
-                Demo
-            </a>
-            <a href={source} className={styles.link}>
-                Source code
-            </a>
+          <a href={demo} className={styles.link}>
+            Demo
+          </a>
+          <a href={source} className={styles.link}>
+            Source code
+          </a>
         </div>
+      </div>
     </div>
   );
 };
